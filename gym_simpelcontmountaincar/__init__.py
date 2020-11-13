f70d96856a10742c3e444aa416fe0cab494f8c96
Johan Sjögren <johan.sjogren@neodev.se>
2020-11-13 13:27:31 +0100

    Update to registry

--- gym_simpelcontmountaincar/__init__.py
+++ gym_simpelcontmountaincar/__init__.py
@@ -3,4 +3,6 @@ from gym.envs.registration import register
 register(
     id='SimpleContMountainCar-v0',
     entry_point='gym_simplecontmountaincar.envs:SimpleMountainCar',
+    max_episode_steps=999,
+    reward_threshold=90.0,
 )
