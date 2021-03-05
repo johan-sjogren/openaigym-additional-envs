46635c1a8face92984c2ca091670c899ee75742f
Johan Sjögren <38350957+johan-sjogren@users.noreply.github.com>
2021-03-05 12:41:00 +0100

    Changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup
 
-setup(name='gym_simplecontmountaincar',
+setup(name='openaigym_additional_envs',
       version='0.0.1',
       install_requires=['gym'],  # And any other dependencies foo needs
       url='https://github.com/neodev-ab/OpenAIGym-additional-envs.git',
