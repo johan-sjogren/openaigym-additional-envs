745ed0a3b6ffb0fcde5a19b2499933fb6232cdb4
Johan Sjögren <38350957+johan-sjogren@users.noreply.github.com>
2021-03-05 12:39:37 +0100

    Changed setup url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='gym_simplecontmountaincar',
       version='0.0.1',
       install_requires=['gym'],  # And any other dependencies foo needs
-      url='https://github.com/neodev-ab/gym-simplecontmountaincar.git',
+      url='https://github.com/neodev-ab/OpenAIGym-additional-envs.git',
       author='Johan Sjögren',
       author_email='johan.sjogren@neodev.se'
 )
